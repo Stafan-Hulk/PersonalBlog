2d506dc2e3344a67b3e8799a3dc70bd4a5a8e030
Stafan_Hunk <963440475@qq.com>
2020-03-01 01:01:07 +0800

    第二次提交

--- dao/dbutil.js
+++ dao/dbutil.js
@@ -2,7 +2,7 @@ let mysql = require("mysql");
 
 function createConnection() {
     let connection = mysql.createConnection({
-        host: "192.168.0.1",
+        host: "192.168.0.109",
         port: "3306",
         user: "root",
         password: "Liuwenbing123",
